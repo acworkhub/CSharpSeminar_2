89ce3b9cae68ce43ab74a8698b4114a7307abb0c
Aleksey Smirnov <acwork@list.ru>
2022-09-05 21:45:18 +0300

    Start Seminar2

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// int[] arr = new int[3];
+// int length = arr.Length;
+// int i = 0;
+
+
+// while (i<length)
+// {
+//     arr[i] = new Random().Next(10,99);
+//     System.Console.WriteLine(arr[i]);
+//     i++;
+// }
+
+
+int[] ar = {3,5,7};
+int count = 0;
+int length = ar.Length;
+int max = 0;
+// System.Console.WriteLine(max);
+
+ while (count<length)
+ {
+    if (ar[count]>ar[max]) 
+   {
+        ar[count] = max;
+        System.Console.WriteLine(ar[max]);
+   } 
+    count++;
+ }
+   System.Console.WriteLine(ar[max]);
+   
+
+
