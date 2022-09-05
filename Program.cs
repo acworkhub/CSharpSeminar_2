15ebfe92e611e4eaa3f06c89c30e3e6842055731
Aleksey Smirnov <acwork@list.ru>
2022-09-06 00:35:43 +0300

    Add Example

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
 ﻿
-Random rnd = new Random();
-int n = new Random().Next(10,100);
-int d1 = n/10;
-int d2 = n % 10;
+// Random rnd = new Random();
+// int n = new Random().Next(10,100);
+// int d1 = n/10;
+// int d2 = n % 10;
 
-int max = d1;
+// int max = d1;
 
-if (max<d2)
-{
-    max=d2;
-}
+// if (max<d2)
+// {
+//     max=d2;
+// }
 
-System.Console.WriteLine($"Максимальная цифра числа {n} => {max}");
+// System.Console.WriteLine($"Максимальная цифра числа {n} => {max}");
 
 // int[] arr = new int[3];
 // int length = arr.Length;
@@ -27,3 +27,90 @@ System.Console.WriteLine($"Максимальная цифра числа {n} =>
 
 
 
+// 456->46
+// 782-72
+
+// int n = new Random().Next(100,1000);
+
+
+//  int d1 = n / 100;
+//  int d2 = n % 10;
+//  int d3 = (n % 100) % 10 ;
+
+//  System.Console.Write($"Преобразование числа {n} =>{d1}{d3}");
+
+
+// int n = new Random().Next(100,1000);
+
+// int resault = n/100*10+ n % 10;
+
+
+//  int d1 = n / 100;
+//  int d2 = n % 10;
+//  int d3 = (n % 100) % 10 ;
+
+//  System.Console.WriteLine("Введите первое число");
+//     int a = Convert.ToInt32(Console.ReadLine());
+//  System.Console.WriteLine("Введите делитель");
+//     int b = Convert.ToInt32(Console.ReadLine());
+
+//     if (a % b == 0)
+//     {
+//         System.Console.WriteLine("Число кратно");
+//     }
+
+//     else
+//     {
+//         System.Console.WriteLine($"Число не кратно{a % b}");
+//     }
+
+
+// System.Console.WriteLine("Введите первое число");
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     int b = 7;
+//     int c = 23;
+
+//     if (a % b == 0 & a % c == 0)
+//     {
+//         System.Console.WriteLine("Число кратно");
+//     }
+
+//     else
+//     {
+//         System.Console.WriteLine($"Число не кратно, остаток  от 7=> {a % b}, остаток от 23=> {a % c}");
+//     }
+
+int a = 42;
+
+if (a % 7 == 0)
+{
+    if (a % 23 == 0)
+    {
+        System.Console.WriteLine("Yes");
+
+
+    }
+
+}
+else 
+    {
+
+        System.Console.WriteLine("No");
+
+    }
+
+// System.Console.WriteLine("Введите возводимое в квадрат число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите проверяемое число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a*a == b)
+//     {
+//         System.Console.WriteLine($"Число {b} является квадратом числа {a} => {a}x{a}={b}");
+//     }
+// else
+//     {
+
+//         System.Console.WriteLine($"Число {b} не является квадратом числа {a}");
+//     }
+
+// Вывод на экран  - - System.Console.WriteLine(""); с переносом значения на другую строку.
