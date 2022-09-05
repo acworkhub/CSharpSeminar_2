f16a98b2aa1bb15b147b0c475f7cfbb2fcf5e927
Aleksey Smirnov <acwork@list.ru>
2022-09-05 22:07:45 +0300

    Add Example1

--- Program.cs
+++ Program.cs
@@ -1,32 +1,29 @@
-﻿// int[] arr = new int[3];
+﻿
+Random rnd = new Random();
+int n = new Random().Next(10,100);
+int d1 = n/10;
+int d2 = n % 10;
+
+int max = d1;
+
+if (max<d2)
+{
+    max=d2;
+}
+
+System.Console.WriteLine($"Максимальная цифра числа {n} => {max}");
+
+// int[] arr = new int[3];
 // int length = arr.Length;
 // int i = 0;
 
 
 // while (i<length)
 // {
-//     arr[i] = new Random().Next(10,99);
+//     arr[i] = new Random().Next(10,100);
 //     System.Console.WriteLine(arr[i]);
 //     i++;
 // }
 
-
-int[] ar = {3,5,7};
-int count = 0;
-int length = ar.Length;
-int max = 0;
-// System.Console.WriteLine(max);
-
- while (count<length)
- {
-    if (ar[count]>ar[max]) 
-   {
-        ar[count] = max;
-        System.Console.WriteLine(ar[max]);
-   } 
-    count++;
- }
-   System.Console.WriteLine(ar[max]);
-   
 
 
